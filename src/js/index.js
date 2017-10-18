9d05f0fe0fa1d34679475a63bd6081b1b35e4fd3
Miguel Vela <miguelvr88@gmail.com>
2017-10-18 22:27:17 +0200

    Base HTML and project setup added
    Background image
    SCSS for colors and commons
    Jquery installed
    	Base for footer
    	Base for header
    	# with '#' will be ignored, and an empty message aborts the commit.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
 /* global $ */
+'use strict'
+
 var $ = require("jquery");
 
 $(document).ready(function ()
 {
+    loadUI()
     console.log('page loaded')
 });
+
+function loadUI () {
+  loadFooter ()
+}
+
+function loadFooter () {
+  // Display current year
+  $('.current-year').html((new Date()).getFullYear())
+}
